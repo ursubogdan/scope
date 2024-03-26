04e446d00171968dd3b148e368129f74caf597f6
ursubogdan <bogdannursu@gmail.com>
2024-03-26 18:37:08 +0200

    create first folder and move files into

---
+++ CH1/index.js
@@ -0,0 +1,34 @@
+warrior = 'Ninja'; // warrior is defined here
+warrior3 = 'Viking';
+
+const screamWarrior = () => {
+    let warrior2 = 'Samurai';
+    // wrapped in a function now is a closure
+    return {
+        shootWarrior: () => {
+            return console.log(warrior, warrior2);
+            
+        }
+    }
+}
+
+const newWarrior = screamWarrior();
+
+newWarrior.shootWarrior();
+
+console.log(warrior, warrior3);
+
+var warrior; // warrior is declared here...
+
+var warrior3; // will show undefined 
+
+
+
+
+// console.log(warrior, warrior2); 
+// -> warrior2 is not defined, cuz is only defined into function.
+
+
+
+
+
