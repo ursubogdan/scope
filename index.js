420b4fce90564d3d2c24a67063e914976ac99898
ursubogdan <bogdannursu@gmail.com>
2024-03-26 17:50:02 +0200

    what is scope

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+var warrior = "Ninja";
+
+const screamWarrior = () => {
+    let warrior2 = "Samurai";
+    console.log(warrior, warrior2);
+}
+
+screamWarrior();
+
+// console.log(warrior, warrior2); -> warrior2 is not defined, cuz is only defined into function.
+
+
+
+
